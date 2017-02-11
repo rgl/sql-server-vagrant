ef9d7749b6ff31d20698abbf3736ca2a2276647c
Rui Lopes <rgl@ruilopes.com>
2017-02-11 08:13:58 +0000

    upgrade the csharp example to dotnet core 1.0.0-rc4-004771

--- examples/csharp/run.ps1
+++ examples/csharp/run.ps1
@@ -3,13 +3,14 @@
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 
 # install the dotnet sdk.
-# see https://blogs.msdn.microsoft.com/dotnet/2017/01/30/announcing-net-core-net-native-and-nuget-updates-in-vs-2017-rc/
-$cliVersion = '1.0.0-rc3-004530'
+# see https://github.com/dotnet/cli/releases/tag/v1.0.0-rc4-004771
+# see https://blogs.msdn.microsoft.com/dotnet/2017/02/07/announcing-net-core-tools-updates-in-vs-2017-rc/
+$cliVersion = '1.0.0-rc4-004771'
 $cliHome = "c:\ProgramData\dotnet-sdk-$cliVersion"
 $archiveName = "dotnet-dev-win-x64.$cliVersion.zip"
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host "Downloading $archiveName..."
-Invoke-WebRequest "https://download.microsoft.com/download/9/7/1/97197CC0-EC7D-45CA-979C-2ABF9B5508A3/$archiveName" -UseBasicParsing -OutFile $archivePath
+Invoke-WebRequest "https://download.microsoft.com/download/B/4/6/B4678511-01F4-4F97-902B-0E58A985932A/$archiveName" -UseBasicParsing -OutFile $archivePath
 Expand-Archive $archivePath -DestinationPath $cliHome
 Remove-Item $archivePath
 
