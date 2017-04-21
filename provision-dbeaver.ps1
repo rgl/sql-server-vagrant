4778102e7cef167a036a53f53d59ca1280973655
Rui Lopes <rgl@ruilopes.com>
2017-04-21 07:54:23 +0100

    configure DBeaver

--- provision-dbeaver.ps1
+++ provision-dbeaver.ps1
@@ -2,5 +2,17 @@
 choco install -y sqljdbc
 
 # install DBeaver.
-choco install -y dbeaver-ee
+choco install -y dbeaver
 
+# configure DBeaver.
+$sqljdbcHome = (Resolve-Path 'C:\Program Files\Microsoft JDBC DRIVER*\sqljdbc*\enu').Path
+$workspaceHome = "$env:USERPROFILE\.dbeaver4"
+$metadataHome = "$workspaceHome\.metadata"
+$pluginsHome = "$metadataHome\.plugins"
+$projectHome = "$workspaceHome\General"
+mkdir -Force "$pluginsHome\org.jkiss.dbeaver.core",$projectHome | Out-Null
+[IO.File]::WriteAllText(
+    "$pluginsHome\org.jkiss.dbeaver.core\drivers.xml",
+    ([IO.File]::ReadAllText("$PWD\provision-dbeaver-drivers.xml") `
+        -replace '@@SQLJDBC_HOME@@',$sqljdbcHome))
+Copy-Item provision-dbeaver-data-sources.xml "$projectHome\.dbeaver-data-sources.xml"
