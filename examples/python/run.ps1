fab5c7197fcd5470a32f036a104b9902a2022c6e
Rui Lopes <rgl@ruilopes.com>
2018-07-23 22:41:52 +0100

    upgrade to pyodbc 4.0.23

--- examples/python/run.ps1
+++ examples/python/run.ps1
@@ -1,5 +1,6 @@
 # install python.
-choco install -y python
+# NB as of 2018-07-23 there is no pyodbc binary package for python 3.7+ so stick with python 3.6.x.
+choco install -y python -Version 3.6.6
 
 # update $env:PATH with the recently installed Chocolatey packages.
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
