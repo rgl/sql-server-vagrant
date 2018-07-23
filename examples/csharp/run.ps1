be453699964dce4caba6eb0ecc9821941146ccbb
Rui Lopes <rgl@ruilopes.com>
2018-07-23 22:43:04 +0100

    upgrade to dotnet sdk 2.1.302 (dotnet core 2.1.2)

--- examples/csharp/run.ps1
+++ examples/csharp/run.ps1
@@ -4,9 +4,9 @@ if (!(Get-Command -ErrorAction SilentlyContinue dotnet.exe)) {
     $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 
     # install the dotnet sdk.
-    # see https://github.com/dotnet/core/blob/master/release-notes/download-archives/2.0.2-sdk-download.md
-    $archiveUrl = 'https://download.microsoft.com/download/7/3/A/73A3E4DC-F019-47D1-9951-0453676E059B/dotnet-sdk-2.0.2-win-x64.exe'
-    $archiveHash = '0b3682c4c7b3b81906d3f011d8a45e06ec2470721a21693ad8040502f201008161f1d4ad73e05763386272c50bfe90be4e2a660734096b4d52e4997bb618a80c'
+    # see https://github.com/dotnet/core/blob/master/release-notes/download-archives/2.1.2-download.md
+    $archiveUrl = 'https://download.microsoft.com/download/4/0/9/40920432-3302-47a8-b13c-bbc4848ad114/dotnet-sdk-2.1.302-win-x64.exe'
+    $archiveHash = '30cd9f690f20c435f30430dff5901c778fd708e9ceab85d7a7bacd469524a7c027e9e5dbb951ec0bc8f7cf9bbd88df225159c87b581ff2cbb0fbc241917fc7aa'
     $archiveName = Split-Path -Leaf $archiveUrl
     $archivePath = "$env:TEMP\$archiveName"
     Write-Host "Downloading $archiveName..."
