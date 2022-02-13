f10ef1ab0a0c49c90b367efef9652c491e0cfa9d
Rui Lopes <rgl@ruilopes.com>
2022-02-13 14:38:17 +0000

    upgrade the python example dependencies

--- examples/python/run.ps1
+++ examples/python/run.ps1
@@ -1,5 +1,7 @@
 # install python.
-choco install -y python --version 3.8.5
+# see https://community.chocolatey.org/packages/python
+# TODO upgrade to python 3.10+ when https://github.com/mkleehammer/pyodbc/issues/981 is fixed.
+choco install -y python --version 3.8.10
 
 # update $env:PATH with the recently installed Chocolatey packages.
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
