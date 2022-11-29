910ec326a552d0330d67fa5f1363cb9ea035001c
Rui Lopes <rgl@ruilopes.com>
2022-11-29 08:53:55 +0000

    upgrade to sql server management studio 18.12.1

--- provision-sql-server-management-studio.ps1
+++ provision-sql-server-management-studio.ps1
@@ -1,2 +1,2 @@
 # see https://community.chocolatey.org/packages/sql-server-management-studio
-choco install -y sql-server-management-studio --version 15.0.18390.0 # SQL Server Management Studio 18.10.
+choco install -y sql-server-management-studio --version 15.0.18424.0 # SQL Server Management Studio 18.12.1.
