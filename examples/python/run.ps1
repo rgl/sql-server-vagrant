29bc8e0c4784b370c472fa0d3814b9fba64943d5
Rui Lopes <rgl@ruilopes.com>
2022-11-29 08:53:05 +0000

    upgrade to python 3.11.0

--- examples/python/run.ps1
+++ examples/python/run.ps1
@@ -1,7 +1,6 @@
 # install python.
 # see https://community.chocolatey.org/packages/python
-# TODO upgrade to python 3.10+ when https://github.com/mkleehammer/pyodbc/issues/981 is fixed.
-choco install -y python --version 3.8.10
+choco install -y python --version 3.11.0
 
 # update $env:PATH with the recently installed Chocolatey packages.
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
