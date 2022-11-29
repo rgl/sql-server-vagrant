16e95d5a54c8168c444fc95527397abdc7f56b86
Rui Lopes <rgl@ruilopes.com>
2022-11-29 08:55:02 +0000

    upgrade to gradle 7.5.1

--- examples/java/run.ps1
+++ examples/java/run.ps1
@@ -5,7 +5,7 @@ $useWindowsTrustStore = $true
 # see https://community.chocolatey.org/packages/temurin11
 # see https://community.chocolatey.org/packages/gradle
 choco install -y temurin11
-choco install -y gradle --version 7.3.3
+choco install -y gradle --version 7.5.1
 
 # update $env:PATH with the recently installed Chocolatey packages.
 Import-Module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
