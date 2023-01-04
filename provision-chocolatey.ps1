d71e004dbc04203433a24005dcf8704361b97b8f
Rui Lopes <rgl@ruilopes.com>
2023-01-04 22:09:33 +0000

    remove unneeded parentheses

--- provision-chocolatey.ps1
+++ provision-chocolatey.ps1
@@ -1,4 +1,4 @@
 # see https://community.chocolatey.org/packages/chocolatey
 $env:chocolateyVersion = '1.2.1'
 
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')
