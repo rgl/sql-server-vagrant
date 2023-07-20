1ee96d06bfbfd7e6b547530c2127f1b64f320a5f
Rui Lopes <rgl@ruilopes.com>
2023-07-20 20:52:15 +0100

    upgrade to sql server management studio 19.1.56

--- provision-sql-server-management-studio.ps1
+++ provision-sql-server-management-studio.ps1
@@ -1,2 +1,2 @@
 # see https://community.chocolatey.org/packages/sql-server-management-studio
-choco install -y sql-server-management-studio --version 15.0.18424.0 # SQL Server Management Studio 18.12.1.
+choco install -y sql-server-management-studio --version 19.1.56 # SQL Server Management Studio 19.1.
