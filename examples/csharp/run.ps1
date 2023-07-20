7835ccb45fdb80800ceb8c8a6fdcef889d50347e
Rui Lopes <rgl@ruilopes.com>
2023-07-20 22:22:54 +0100

    upgrade the csharp example dependencies

--- examples/csharp/run.ps1
+++ examples/csharp/run.ps1
@@ -1,14 +1,14 @@
-if (!(Get-Command -ErrorAction SilentlyContinue dotnet.exe)) {
+if (!(Get-Command -ErrorAction SilentlyContinue dotnet.exe) -or !(dotnet --list-sdks)) {
     # see https://dotnet.microsoft.com/en-us/download/dotnet/6.0
-    # see https://github.com/dotnet/core/blob/main/release-notes/6.0/6.0.12/6.0.12.md
+    # see https://github.com/dotnet/core/blob/main/release-notes/6.0/6.0.20/6.0.20.md
 
     # opt-out from dotnet telemetry.
     [Environment]::SetEnvironmentVariable('DOTNET_CLI_TELEMETRY_OPTOUT', '1', 'Machine')
     $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 
     # install the dotnet sdk.
-    $archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/0a672516-37fb-40de-8bef-725975e0b137/a632cde8d629f9ba9c12196f7e7660db/dotnet-sdk-6.0.404-win-x64.exe'
-    $archiveHash = '4e4f9f2ad0c74aa69f488e50c8722caabb55bf8cdeca7bd265713dddf2870d2ba0978798b55d44b0f5ba2ecdd979a6d5faae79cf9645270bd5fd2a21d1fab35d'
+    $archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/6a7d9254-d184-41a1-91fc-00ed876297a9/9db88f1a5fe2375b8c389d7087db132e/dotnet-sdk-6.0.412-win-x64.exe'
+    $archiveHash = '7465163a2681b37bf164c6a257fc72e3865c98ed56a6a1a378302c91a0482a9c3405ca5adae46d3cd93859751b8724a0f67aa942e8a982e52a65306556c2a3ab'
     $archiveName = Split-Path -Leaf $archiveUrl
     $archivePath = "$env:TEMP\$archiveName"
     Write-Host "Downloading $archiveName..."
@@ -26,6 +26,12 @@ if (!(Get-Command -ErrorAction SilentlyContinue dotnet.exe)) {
 
     # reload PATH.
     $env:PATH = "$([Environment]::GetEnvironmentVariable('PATH', 'Machine'));$([Environment]::GetEnvironmentVariable('PATH', 'User'))"
+
+    # add the nuget.org source.
+    # see https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-nuget-add-source
+    Write-Host "Adding the nuget nuget.org source..."
+    dotnet nuget add source --name nuget.org https://api.nuget.org/v3/index.json
+    dotnet nuget list source
 }
 
 # show information about dotnet.
