8e342a5b32abfc76a7ee39bf96c37f72a890c5de
Rui Lopes <rgl@ruilopes.com>
2025-06-18 07:12:54 +0000

    upgrade to sql server management studio 21.2.5

--- provision-sql-server-management-studio.ps1
+++ provision-sql-server-management-studio.ps1
@@ -1,2 +1,2 @@
 # see https://community.chocolatey.org/packages/sql-server-management-studio
-choco install -y sql-server-management-studio --version 19.1.56 # SQL Server Management Studio 19.1.
+choco install -y sql-server-management-studio --version 21.2.5
