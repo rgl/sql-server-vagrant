7877c7a98695da750f36f16207cfcf0ef4d61b94
Rui Lopes <rgl@ruilopes.com>
2025-09-14 07:03:12 +0000

    use the computer name when connecting to the sql server instance

--- examples/powershell/sqlclient.ps1
+++ examples/powershell/sqlclient.ps1
@@ -7,4 +7,4 @@ Write-Host 'SQL Server User Name (integrated Windows authentication credentials)
 Write-Host (SqlExecuteScalar "Server=$env:SQL_SERVER_INSTANCE; Database=master; Integrated Security=true" 'select suser_name()')
 
 Write-Host 'SQL Server User Name (alice.doe; username/password credentials; TCP/IP connection) with .NET SqlClient:'
-Write-Host (SqlExecuteScalar 'Server=localhost,1433; User ID=alice.doe; Password=HeyH0Password; Database=master' 'select suser_name()')
+Write-Host (SqlExecuteScalar "Server=$env:COMPUTERNAME,1433; User ID=alice.doe; Password=HeyH0Password; Database=master" 'select suser_name()')
--- examples/powershell/sqlps.ps1
+++ examples/powershell/sqlps.ps1
@@ -7,4 +7,4 @@ Write-Host 'SQL Server User Name (integrated Windows authentication credentials)
 (Invoke-Sqlcmd -ServerInstance $env:SQL_SERVER_INSTANCE -Query 'select suser_name() as value').value
 
 Write-Host 'SQL Server User Name (alice.doe; username/password credentials; TCP/IP connection):'
-(Invoke-Sqlcmd -ServerInstance 'localhost,1433' -Username alice.doe -Password HeyH0Password -Query 'select suser_name() as value').value
+(Invoke-Sqlcmd -ServerInstance "$env:COMPUTERNAME,1433" -Username alice.doe -Password HeyH0Password -Query 'select suser_name() as value').value
